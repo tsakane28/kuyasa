3f689ab74664d31fd2abbb6fcc0dbb8b1ac6fea8
Munashe Shiri <mshiri@ZINGSERVER1.GOV>
2024-04-29 10:53:35 +0200

    Add project files

---
+++ script.js
@@ -0,0 +1,19 @@
+// Get the navbar
+var navbar = document.querySelector('.navbar');
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Function to add the "sticky" class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNavbar() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+}
+
+// When the user scrolls the page, execute stickyNavbar 
+window.onscroll = function() {
+    stickyNavbar();
+};
